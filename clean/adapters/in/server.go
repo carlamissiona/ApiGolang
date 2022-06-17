a62edaf86524745ba1e4869c9c01adc1e3ba2c3a
missiona.carla <missiona.carla@gmail.com>
2022-06-17 00:19:40 +0000

    error

--- clean/adapters/in/server.go
+++ clean/adapters/in/server.go
@@ -12,14 +12,14 @@ package in
 var Server * gin.Engine
 
 
-    // db_instance *sql.DB
-        // db.Exec()   NOTE *db.Database_Instance  errored
+// db_instance *sql.DB
+// db.Exec()   NOTE *db.Database_Instance  errored
 
-    // log.Println( ports.NewAPIService( db_instance  ) )
+// log.Println( ports.NewAPIService( db_instance  ) )
 
 func NewServer(db_adapter db.Adapter) * gin.Engine {
     Server = gin.Default()
-     
+
 
     Server.GET("/", func(c * gin.Context) {
         if (CheckInitData(db_adapter)) {
@@ -38,25 +38,25 @@ func Start() {
 }
 func CheckInitData(db_adapter db.Adapter) bool {
     // obs, err := db_adapter.Db.QueryRow(`SELECT Count(*) FROM tbl_countrypatient`).Scan(&count)
-var count int32
-    err := db_adapter.Db.QueryRow(`SELECT Count(*) FROM tbl_countrypatient`).Scan(&count)
- 
-    switch {    
-case err != nil:
-       log.Printf("Error CheckInitData" )
-    log.Fatal(err)
-default:
-  log.Printf("Number of rows are %s\n", count)
-}
-  // log.Println("=====obs====");
-    
-  //   log.Println(obs); log.Println(err)
-  //   log.Println("=====obs====");
-  //   if ( obs == nil ){
-  //      log.Println("=====empty obs====");log.Println("=====empty obs====");
-  //   }else{
-  //      log.Println("=====obs has values====");
-  //   }
+    var count int32
+    err := db_adapter.Db.QueryRow(`SELECT Count(*) FROM tbl_countrypatient`).Scan( & count)
+
+    switch {
+        case err != nil:
+            log.Printf("Error CheckInitData")
+            log.Fatal(err)
+        default:
+            log.Printf("Number of rows are %s\n", count)
+    }
+    // log.Println("=====obs====");
+
+    //   log.Println(obs); log.Println(err)
+    //   log.Println("=====obs====");
+    //   if ( obs == nil ){
+    //      log.Println("=====empty obs====");log.Println("=====empty obs====");
+    //   }else{
+    //      log.Println("=====obs has values====");
+    //   }
     return true
 }
 

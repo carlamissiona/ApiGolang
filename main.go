6f21971cb9d58925f56af336df09e4374690d1e5
missiona.carla <missiona.carla@gmail.com>
2022-06-17 10:41:49 +0000

    file

--- main.go
+++ main.go
@@ -12,58 +12,54 @@ import (
 
     "gomorganexam/clean/adapters/in"
     "gomorganexam/clean/adapters/out/db"
-  
+
     "gomorganexam/clean/ports"
     "github.com/joho/godotenv"
 )
 
 
 func main() {
- 
-     if (len(os.Args) == 2) { 
+
+    if (len(os.Args) == 2) {
         if (os.Args[1] == "minimal") {
             log.Println(os.Args[1])
-              
-            err := godotenv.Load(".environ_development")
-          
+
+            err:= godotenv.Load(".environ_development")
+
             if err != nil {
                 log.Fatalf("failed reading env file: %v", err)
             }
-  
+
             db_info:= os.Getenv("POSTGRES_URL")
-          
-            db := db.NewAdapter("postgres", db_info)
-            server := in.NewServer()
-          
+
+            db:= db.NewAdapter("postgres", db_info)
+            server:= in.NewServer()
+
             // csv record
-            apisvc := ports.NewAPIService(db , nil , server)
+            apisvc:= ports.NewAPIService(db, nil, server)
+
+            apisvc.Start(checkInitData(db))
  
-            apisvc.Start(  checkInitData(db) ) 
-           
-          
-          
-
-
         }
     }
     log.Println("Hi Postg API")
 }
-  
 
 
-func checkInitData(db_adapter *db.Adapter) bool {
+
+func checkInitData(db_adapter * db.Adapter) bool {
     // obs, err := db_adapter.Db.QueryRow(`SELECT Count(*) FROM tbl_countrypatient`).Scan(&count)
     var count int32 = 0
-    err := db_adapter.Data.QueryRow(`SELECT Count(*) FROM covid_observations`).Scan( & count)
-    if err != nil{
-         log.Println("=>ERROR!! Check PG DATABASE DATA") 
-           log.Println(err)
-            log.Println("=>END OF ERROR!! Check PG DATABASE DATA") 
-            return false      
+    err:= db_adapter.Data.QueryRow(`SELECT Count(*) FROM covid_observations`).Scan(&count)
+    if err != nil {
+        log.Println("=>ERROR!! Check PG DATABASE DATA")
+        log.Println(err)
+        log.Println("=>END OF ERROR!! Check PG DATABASE DATA")
+        return false
     }
-     
-      log.Printf("=>DEBUG!! Number of rows are %s\n", count)
-      log.Printf("=>DEBUG!! Number of rows are %s\n", count)
-    
+
+    log.Printf("=>DEBUG!! Number of rows are %s\n", count)
+    log.Printf("=>DEBUG!! Number of rows are %s\n", count)
+
     return true
 }
